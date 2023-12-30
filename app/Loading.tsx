7e7e33b00840084de36dd5142c5baa23e550ded2
bnt10 <bnt10@naver.com>
2023-12-30 12:42:32 +0900

    chore: fix lint

--- app/Loading.tsx
+++ app/Loading.tsx
@@ -1,14 +1,15 @@
 // SkeletonLoader.tsx
-import React from 'react'
 import 'tailwindcss/tailwind.css'
+
+import React from 'react'
 
 export default function Loading() {
   return (
-    <div className="animate-pulse flex space-x-4">
+    <div className="flex animate-pulse space-x-4">
       <div className="flex-1 space-y-6 py-1">
-        <div className="h-2 bg-gray-300 rounded"></div>
+        <div className="h-2 rounded bg-gray-300"></div>
         <div className="space-y-3">
-          <div className="h-2 bg-gray-300 rounded"></div>
+          <div className="h-2 rounded bg-gray-300"></div>
         </div>
       </div>
     </div>
