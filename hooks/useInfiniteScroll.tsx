5db1480bf16aaa92cce86a22d343689c5297fa13
bnt10 <bnt10@naver.com>
2023-12-30 22:55:41 +0900

    feat: add infinitieScroll hook

---
+++ hooks/useInfiniteScroll.tsx
@@ -0,0 +1,56 @@
+// useInfiniteScroll.ts
+import type { RefObject } from 'react'
+import { useEffect, useRef, useState } from 'react'
+
+interface UseInfiniteScrollProps {
+  target: RefObject<HTMLDivElement | null> // or HTMLElement, depending on your use case
+  parent?: Element | null // Parent element for the Intersection Observer
+  threshold?: number // Threshold for the Intersection Observer
+  rootMargin?: string // Root margin for the Intersection Observer
+  callback: () => void // Callback function to be called when the target is intersecting
+}
+
+const useInfiniteScroll = ({
+  target,
+  parent = null,
+  threshold = 0.1,
+  rootMargin = '0px',
+  callback,
+}: UseInfiniteScrollProps): boolean => {
+  const [isIntersecting, setIsIntersecting] = useState<boolean>(false)
+  const observer = useRef<IntersectionObserver | null>(null)
+
+  useEffect(() => {
+    if (target && !target.current) {
+      return
+    }
+    const opts: IntersectionObserverInit = {
+      root: parent,
+      rootMargin,
+      threshold,
+    }
+    observer.current = new IntersectionObserver((entries) => {
+      const [entry] = entries
+      if (entry.isIntersecting) {
+        setIsIntersecting(true)
+        callback()
+      } else {
+        setIsIntersecting(false)
+      }
+    }, opts)
+
+    if (target.current) {
+      observer.current.observe(target.current)
+    }
+
+    return () => {
+      if (observer.current) {
+        observer.current.disconnect()
+      }
+    }
+  }, [target, parent, threshold, rootMargin, callback])
+
+  return isIntersecting
+}
+
+export default useInfiniteScroll
