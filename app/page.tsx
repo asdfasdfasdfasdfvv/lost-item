d34da6504e1a911c7f80a42735e7177ded3d2437
bnt10 <bnt10@naver.com>
2023-12-30 12:49:54 +0900

    style: fix layout padding

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import LostItems from 'components/lost-item/LostItems'
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-between p-24">
+    <main className="flex flex-col items-center justify-between p-24pxr">
       <LostItems />
     </main>
   )
