fd754857f4543205eb90854f08a84628b9028665
jin <bnt10@naver.com>
2024-01-14 12:56:00 +0900

    feat: add url parm builder

---
+++ utils/api.ts
@@ -0,0 +1,30 @@
+export function buildUrlWithParams(
+  base: string,
+  params: Record<string, string | undefined>
+) {
+  const filteredParams = Object.entries(params).filter(
+    ([, value]) => value !== undefined
+  )
+  if (filteredParams.length === 0) return base
+
+  const paramString = filteredParams
+    .map(([key, value]) => `${key}=${value}`)
+    .join('&')
+  return `${base}?${paramString}`
+}
+
+export function buildUrlWithPathParams(
+  base: string,
+  params: Record<string, string | undefined>
+) {
+  if (!params) {
+    return { url: base, error: new Error('No Params') }
+  }
+  let url = base
+  for (const [key, value] of Object.entries(params)) {
+    if (value !== undefined) {
+      url = url.replace(`:${key}`, value)
+    }
+  }
+  return { url, error: null }
+}
